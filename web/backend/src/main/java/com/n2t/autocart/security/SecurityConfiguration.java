8775e6a15f0e200a342ec97b6ff89717578ce79a
thanhtung900913 <thanhtung2962004@gmail.com>
2026-03-24 16:39:47 +0700

    feat(security): configure security and add security utilities

--- web/backend/src/main/java/com/n2t/autocart/security/SecurityConfiguration.java
+++ web/backend/src/main/java/com/n2t/autocart/security/SecurityConfiguration.java
@@ -1,12 +1,28 @@
 package com.n2t.autocart.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SecurityConfiguration {
+    @Value("${auto-cart.jwt.base64-secret}")
+    private String jwtKey;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -16,5 +32,39 @@ public class SecurityConfiguration {
                 );
 
         return http.build();
+    }
+    private SecretKey getSecretKey(){
+        byte[] keyBytes = Base64.from(this.jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(
+                getSecretKey()).macAlgorithm(SecurityUtil.JWT_ALGORITHM).build();
+        return token -> {
+            try {
+                return jwtDecoder.decode(token);
+            }catch (Exception e){
+                System.out.println("JWT error"+e.getMessage());
+                throw e;
+            }
+        };
+    }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
---
+++ web/backend/src/main/java/com/n2t/autocart/security/SecurityUtil.java
@@ -0,0 +1,164 @@
+package com.n2t.autocart.security;
+
+import com.n2t.autocart.modules.account.dto.LoginResponseDTO;
+import com.nimbusds.jose.util.Base64;
+import lombok.Getter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import java.util.UUID;
+
+@Getter
+@Service
+public class SecurityUtil {
+    private final JwtEncoder jwtEncoder;
+    @Value("${auto-cart.jwt.base64-secret}")
+    private String jwtKey;
+
+    @Value("${auto-cart.jwt.access-token-validity-in-seconds}")
+    private int accessAxpiredTime;
+
+    @Value("${auto-cart.jwt.refresh-token-validity-in-seconds}")
+    private int refreshAxpiredTime;
+
+    public static final MacAlgorithm JWT_ALGORITHM = MacAlgorithm.HS256;
+
+    public SecurityUtil(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+    public String createAccessToken(String email, LoginResponseDTO.UserResponse userResponse){
+        Instant now = Instant.now();
+        Instant validity = now.plus(this.accessAxpiredTime, ChronoUnit.SECONDS);
+
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuedAt(now)
+                .expiresAt(validity)
+                .subject(email)
+                .claim("user", userResponse)
+                .build();
+        JwsHeader jwsHeader = JwsHeader.with(JWT_ALGORITHM).build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).getTokenValue();
+    }
+
+    public String createRefreshToken(String email, LoginResponseDTO loginResponseDTO){
+        Instant now = Instant.now();
+        Instant validity = now.plus(this.refreshAxpiredTime, ChronoUnit.SECONDS);
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .id(UUID.randomUUID().toString())
+                .issuedAt(now)
+                .expiresAt(validity)
+                .subject(email)
+                .claim("user", loginResponseDTO.getUserResponse())
+                .build();
+        JwsHeader jwsHeader = JwsHeader.with(JWT_ALGORITHM).build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).getTokenValue();
+    }
+    private SecretKey getSecretKey(){
+        byte[] keyBytes = Base64.from(this.jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
+    }
+
+    public Jwt verifyRefreshToken(String refreshToken) {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(
+                getSecretKey()).macAlgorithm(SecurityUtil.JWT_ALGORITHM).build();
+        try {
+            return jwtDecoder.decode(refreshToken);
+        }catch (Exception e){
+            System.out.println("JWT error"+e.getMessage());
+            throw e;
+        }
+    }
+    /**
+     * Get the login of the current user.
+     *
+     * @return the login of the current user.
+     */
+    public static Optional<String> getCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(extractPrincipal(securityContext.getAuthentication()));
+    }
+
+    private static String extractPrincipal(Authentication authentication) {
+        if (authentication == null) {
+            return null;
+        } else if (authentication.getPrincipal() instanceof UserDetails springSecurityUser) {
+            return springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof Jwt jwt) {
+            return jwt.getSubject();
+        } else if (authentication.getPrincipal() instanceof String s) {
+            return s;
+        }
+        return null;
+    }
+//
+//    /**
+//     * Get the JWT of the current user.
+//     *
+//     * @return the JWT of the current user.
+//     */
+//    public static Optional<String> getCurrentUserJWT() {
+//        SecurityContext securityContext = SecurityContextHolder.getContext();
+//        return Optional.ofNullable(securityContext.getAuthentication())
+//                .filter(authentication -> authentication.getCredentials() instanceof String)
+//                .map(authentication -> (String) authentication.getCredentials());
+//    }
+//
+//    /**
+//     * Check if a user is authenticated.
+//     *
+//     * @return true if the user is authenticated, false otherwise.
+//     */
+////    public static boolean isAuthenticated() {
+////        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+////        return authentication != null && getAuthorities(authentication).noneMatch(AuthoritiesConstants.ANONYMOUS::equals);
+////    }
+//
+//    /**
+//     * Checks if the current user has any of the authorities.
+//     *
+//     * @param authorities the authorities to check.
+//     * @return true if the current user has any of the authorities, false otherwise.
+//     */
+//    public static boolean hasCurrentUserAnyOfAuthorities(String... authorities) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        return (
+//                authentication != null && getAuthorities(authentication).anyMatch(authority -> Arrays.asList(authorities).contains(authority))
+//        );
+//    }
+//
+//    /**
+//     * Checks if the current user has none of the authorities.
+//     *
+//     * @param authorities the authorities to check.
+//     * @return true if the current user has none of the authorities, false otherwise.
+//     */
+//    public static boolean hasCurrentUserNoneOfAuthorities(String... authorities) {
+//        return !hasCurrentUserAnyOfAuthorities(authorities);
+//    }
+//
+//    /**
+//     * Checks if the current user has a specific authority.
+//     *
+//     * @param authority the authority to check.
+//     * @return true if the current user has the authority, false otherwise.
+//     */
+//    public static boolean hasCurrentUserThisAuthority(String authority) {
+//        return hasCurrentUserAnyOfAuthorities(authority);
+//    }
+//
+//    private static Stream<String> getAuthorities(Authentication authentication) {
+//        return authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority);
+//    }
+}
