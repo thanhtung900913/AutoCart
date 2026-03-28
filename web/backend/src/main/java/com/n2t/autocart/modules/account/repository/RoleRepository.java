bb3a51cc569a0ba0025214e3c1afbf15ab85cb16
thanhtung900913 <thanhtung2962004@gmail.com>
2026-03-28 21:22:43 +0700

    feat(account): update role repository for role retrieval

--- web/backend/src/main/java/com/n2t/autocart/modules/account/repository/RoleRepository.java
+++ web/backend/src/main/java/com/n2t/autocart/modules/account/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package com.n2t.autocart.modules.account.repository;
 import com.n2t.autocart.modules.account.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    List<Role> findDistinctByUserRoles_User_Email(String email);
 }
