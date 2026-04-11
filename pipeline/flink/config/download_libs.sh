3b459f8a7b2f4e30501120a77b2c7139c0a64af4
thanhtung900913 <thanhtung2962004@gmail.com>
2026-04-11 12:23:29 +0700

    fix: change config of flink connection

--- pipeline/flink/config/download_libs.sh
+++ pipeline/flink/config/download_libs.sh
@@ -5,5 +5,5 @@ mkdir -p ./lib
 curl -o ./lib/flink-connector-jdbc-3.1.2-1.18.jar https://repo1.maven.org/maven2/org/apache/flink/flink-connector-jdbc/3.1.2-1.18/flink-connector-jdbc-3.1.2-1.18.jar
 curl -o ./lib/clickhouse-jdbc-0.3.2.jar https://repo1.maven.org/maven2/ru/yandex/clickhouse/clickhouse-jdbc/0.3.2/clickhouse-jdbc-0.3.2.jar
 curl -o ./lib/flink-sql-connector-kafka-3.1.0-1.18.jar https://repo.maven.apache.org/maven2/org/apache/flink/flink-sql-connector-kafka/3.1.0-1.18/flink-sql-connector-kafka-3.1.0-1.18.jar
-
+curl -o ./lib/flink-json-1.18.1.jar https://repo.maven.apache.org/maven2/org/apache/flink/flink-json/1.18.1/flink-json-1.18.1.jar
 echo "Libs downloaded successfully"
